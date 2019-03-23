7a8ca1ec8ad42f8942245f96d8d68c3161dc4c30
Nic Wiggins <wiggins.nic@gmail.com>
2019-03-23 00:31:48 -0400

    python to launch on server that has year of twitter data, just added type errror handling since some lines are just numbers

---
+++ project/findTesla.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+directory = '/user/research/ptan/data/Twitter/'
+
+testList = []
+paths = []
+keyword = 'tesla'
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        paths.append(directory+file)
+
+paths
+print(len(paths))
+
+for i in range(len(paths)):
+    print('file',i,'of',len(paths))
+    with open(paths[i]) as f:
+        for line in f:
+            try:
+                c = json.loads(line)
+                if 'text' in c and keyword in str(c['text']).lower():
+                    testList.append({i: c[i] for i in ('created_at', 'text')})
+            except ValueError:
+                pass
+            except TypeError:
+            	pass
+
+
+a = json.dumps(testList)
+with open('final.json', 'w') as outfile:
+    json.dump(a, outfile)
+
