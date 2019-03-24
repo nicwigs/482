2941e80961022b35cbe7bbb2353282273b5ce6ab
Nic Wiggins <wiggins.nic@gmail.com>
2019-03-24 00:53:58 -0400

    school server ran out of space doing all 7960 files, going to do 1000 at a time.

--- project/findTesla.py
+++ project/findTesla.py
@@ -13,7 +13,7 @@ for root, dirs, files in os.walk(directory):
 paths
 print(len(paths))
 
-for i in range(len(paths)):
+for i in range(0,1000):
     print('file',i,'of',len(paths))
     with open(paths[i]) as f:
         for line in f:
