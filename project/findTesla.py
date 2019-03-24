e9114e5ae55dee1280641e3ef65ca357122a8b52
Nic Wiggins <wiggins.nic@gmail.com>
2019-03-24 08:10:42 -0400

    have to do only few files at a time

--- project/findTesla.py
+++ project/findTesla.py
@@ -13,7 +13,7 @@ for root, dirs, files in os.walk(directory):
 paths
 print(len(paths))
 
-for i in range(0,1000):
+for i in range(0,100):
     print('file',i,'of',len(paths))
     with open(paths[i]) as f:
         for line in f:
