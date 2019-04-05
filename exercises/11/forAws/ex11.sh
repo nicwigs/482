818456c4088d56f11331c7b46200dc444b029389
Nic Wiggins <wiggins.nic@gmail.com>
2019-04-04 23:52:37 -0400

    further comments

--- exercises/11/forAws/ex11.sh
+++ exercises/11/forAws/ex11.sh
@@ -10,9 +10,13 @@ cd exercise11
 #exec exercise11.pig
 
 cp ../exercise11.pig .
+
+# upload to hdfs
 hadoop fs -copyFromLocal wiki_edit.txt .
 hadoop fs -copyFromLocal nolog.conf .
 
+# no log file, run in local mode
 pig -x local -4 nolog.conf exercise11.pig
 
+# since in local mode, output not stored on hdfs, hence no -getmerge
 cat output/* > result.txt
