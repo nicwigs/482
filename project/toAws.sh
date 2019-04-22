a53b0958518f5ad92fb5ade6d47790e5c795ab31
Nic Wiggins <wiggins.nic@gmail.com>
2019-04-22 11:47:23 -0400

    copy to aws

---
+++ project/toAws.sh
@@ -0,0 +1,3 @@
+scp -i ../../cse482.pem sent.sh hadoop@ec2-18-191-5-200.us-east-2.compute.amazonaws.com:/home/hadoop/
+scp -i ../../cse482.pem getSentiment.java hadoop@ec2-18-191-5-200.us-east-2.compute.amazonaws.com:/home/hadoop/
+scp -i ../../cse482.pem ../../project/data/opinion/train.data hadoop@ec2-18-191-5-200.us-east-2.compute.amazonaws.com:/home/hadoop/
